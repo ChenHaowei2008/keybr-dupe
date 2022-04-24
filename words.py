17b57788996c3588f378568d93823f6695c75546
chenhaowei2008 <haoweichenchw@gmail.com>
2022-04-24 10:56:13 +0800

    removed excessive code from the word generator

--- words.py
+++ words.py
@@ -1,15 +1,13 @@
-from random import choice
-import time
-times = []
-for i in range(100000):
-    startTime = time.time()
-    vowels = ('a', 'e', 'i', 'o', 'u')
-    consts = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'qu', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'tt', 'ch', 'sh')
-    isVowel = False
-    word = ''
-    for i in range(5):
-        if(isVowel): word += choice(vowels)
-        else: word += choice(consts)
-        isVowel = not isVowel
-    times.append(time.time() - startTime)
-print(sum(times)/len(times))
+import random 
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+consts = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'qu', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'tt', 'ch', 'sh')
+isVowel = False
+length = 5
+word = ''
+
+for i in range(length):
+    if(isVowel): word += random.choice(vowels)
+    else: word += random.choice(consts)
+    isVowel = not isVowel
+print(word)
