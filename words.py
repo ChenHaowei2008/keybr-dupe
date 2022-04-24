f7d205b678796b979671cb02bcac2574f6db221d
chenhaowei2008 <haoweichenchw@gmail.com>
2022-04-24 11:28:09 +0800

    added weightings for alphabet and some comments

--- words.py
+++ words.py
@@ -1,14 +1,15 @@
 import random 
+def generateWord(length, vowels, consts, vowelsWeight, constsWeight)
+    #vowels = ('a', 'e', 'i', 'o', 'u')
+    #vowelsWeight = (21.87, 34.67, 28.67, 20, 6.67)
+    #consts = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'tt', 'ch', 'sh', 'qu')
+    word = ''
 
-vowels = ('a', 'e', 'i', 'o', 'u')
-consts = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'qu', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'tt', 'ch', 'sh')
-isVowel = False
-length = 5
-word = ''
-
-for i in range(length):
-    if(random.randint(1,100) > 38):
-        word += random.choice(consts)
-    else: 
-        word += random.choice(vowels)
-print(word)
+    for i in range(length):
+        #38 percent of english characters are vowels
+        #More accurate because yes.
+        if(random.randint(1,100) > 38):
+            word += ''.join(random.choices(consts, weights=constsWeight))
+        else: 
+            word += ''.join(random.choices(vowels, weights=vowelsWeight))
+    return word
